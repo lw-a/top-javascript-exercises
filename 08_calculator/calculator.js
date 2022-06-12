edb1cda59ca56b5fc9bfe6c338c41686bc8d9528
Adam Hemming <hemming.dev@gmail.com>
2022-06-12 23:05:35 +0900

    Finish task 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(x) {
+	let total = 0;
+
+    for (let i = 0; i < x.length; i++) {
+      total += x[i];
+    }
+    return total;
+
 };
 
-const multiply = function() {
+const multiply = function(x) {
+  let total = 1;
 
+    for (let i = 0; i < x.length; i++) {
+      total *= x[i];
+    }
+    
+  return total;
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	return Math.pow(x, y);
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let total = 1;
+
+  for (i = x; i > 0; i--) {
+    total *= i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
