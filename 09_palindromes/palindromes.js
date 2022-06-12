7ff49c839f031202f724ce76032dcfc0d8825c30
Adam Hemming <hemming.dev@gmail.com>
2022-06-12 23:51:00 +0900

    Finish task 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+
+    let cleanString = str.toLowerCase();
+    cleanString = cleanString.replace(/[., /#!$%^&*;:{}=-_`~()]/g, "");
+    let newString = "";
+
+    for (let i = cleanString.length - 1; i >= 0; i--) {
+        newString += cleanString[i]
+    }
+    
+    if (cleanString === newString) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
