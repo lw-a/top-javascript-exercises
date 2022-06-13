fb8e586d34d18ccc654a1a092708cdc237a65eac
Adam Hemming <hemming.dev@gmail.com>
2022-06-13 10:12:09 +0900

    Finish task 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
 
+if (x < 0) {
+    return "OOPS";
+}
+
+let total = 1;
+let current = 1;
+let previous = 0;
+
+for (i = 1; i < x; i++) {
+
+total = previous + current;
+previous = current;
+current = total;
+
+}
+
+return total;
 };
 
 // Do not edit below this line
