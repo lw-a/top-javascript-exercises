9302fac18e2fc4e57af50459417ceea5756b15e3
Adam Hemming <hemming.dev@gmail.com>
2022-06-14 11:26:41 +0900

    Finish task 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+let peopleOldest = people.sort(function(a,b) {
+
+    if (a.yearOfDeath === undefined) {
+        a.yearOfDeath = new Date().getFullYear()
+    }
+
+    let lastPerson = a.yearOfDeath - a.yearOfBirth;
+
+    if (b.yearOfDeath === undefined) {
+        b.yearOfDeath = new Date().getFullYear()
+    }
+
+    let nextPerson = b.yearOfDeath - b.yearOfBirth;
+
+    return lastPerson > nextPerson ? -1 : 1;
+});
+
+return peopleOldest[0];
 
 };
 
